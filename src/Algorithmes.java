9a458e5a6d4327ad7641cdff3727c5ca69ccb1a2
hotman <truffechocolatresse@hotmail.fr>
2018-03-29 19:53:36 +0200

    last

--- src/Algorithmes.java
+++ src/Algorithmes.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Classe contenant les algorithmes
@@ -21,11 +24,43 @@ public class Algorithmes {
 	private List<Fournisseur> fournisseursRetenus;
 	
 	/**
+	 * Seos-ensemble Y
+	 * */
+	private Map<Integer, Boolean> clientsY;
+	
+	/**
+	 * Les fournisseurs ouverts et fermés
+	 * */
+	private Map<Integer, Boolean> fournisseursOuvertsFermes;
+	
+	/**
+	 * Liste des clients (connectés et non connectés)
+	 * */
+	private Map<Integer, Boolean> lesClients;
+	
+	/**
 	 * Constructeur
 	 * */
 	public Algorithmes(List<Fournisseur> fournisseurs) {
 		this.fournisseurs = fournisseurs;
 		this.fournisseursRetenus = new ArrayList<>();
+		this.lesClients = new HashMap<>();
+		this.fournisseursOuvertsFermes = new HashMap<>();
+		this.clientsY = new HashMap<>();
+		trierCoutConnexion(getFournisseurs());
+		//dans un premier temps, aucun fournisseur connecté
+		int i = 0;
+		for (int cout : getFournisseurs().get(0).getCoutsClients()) {
+			lesClients.put(i, false);
+			i++;
+		}
+		
+		//dans un premier temps, aucun fournisseur ouvert
+		i = 0;
+		for (Fournisseur fournisseur : getFournisseurs()) {
+			fournisseursOuvertsFermes.put(i, false);
+			i++;
+		}
 	}
 	
 	/**
@@ -41,7 +76,7 @@ public class Algorithmes {
 			sommeCoutsOuvs += O.get(i).getCoutOuverture();
 		}
 		
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < getLesClients().size(); i++) {
 			minCliCourant = Integer.MAX_VALUE;
 			for (int j = 0; j < O.size(); j++) {
 				if(O.get(j).getCoutsClients().get(i) < minCliCourant) {
@@ -53,6 +88,7 @@ public class Algorithmes {
 		
 		return sommeCoutsOuvs+sommeCoutsconnexClis;
 	}
+	
 
 	/**
 	 * Algorithme 1 : Glouton
@@ -103,6 +139,163 @@ public class Algorithmes {
 		
 		return getFournisseursRetenus();
 	}
+
+	/**
+	 * Trie les couts des clients par fournisseur par ordre croissant
+	 * */
+	public void trierCoutConnexion(List<Fournisseur> fournisseurs) {
+		for (Fournisseur fournisseur : fournisseurs) {
+			Collections.sort(fournisseur.getCoutsClients());
+		}
+	}
+	
+	/**
+	 * Alpha : le client (pas connecté) qui a la cout minimum parmis les fournisseurs ouverts
+	 * */
+	public int alpha() {
+		int min = Integer.MAX_VALUE;
+
+		for (int idClient : getLesClients().keySet()) {
+			if(!getLesClients().get(idClient)) { //client non connecté
+				for (Fournisseur fournisseur: getFournisseursRetenus()) {
+					if(fournisseur.getCoutsClients().get(idClient) < min) {
+						min = fournisseur.getCoutsClients().get(idClient); 
+					}
+				}
+			}
+		}
+		
+		return min;
+	}
+	
+	/**
+	 * le cout d'un client parmis les fournisseurs ouverts 
+	 * */
+	public int meilleurCoutClientFournisseursOuverts(int idClient) {
+		int minClientJFournisseursOuverts = Integer.MAX_VALUE;
+		
+		if (getFournisseursRetenus().isEmpty()) {
+			minClientJFournisseursOuverts=0;
+		}
+		
+		//les fournisseurs ouverts
+		for (Fournisseur fournisseur: getFournisseursRetenus()) {
+			if(fournisseur.getCoutsClients().get(idClient) < minClientJFournisseursOuverts) { //un fournisseur avec cout meilleur pour ce client
+				minClientJFournisseursOuverts = fournisseur.getCoutsClients().get(idClient); 
+			}
+		}
+		return minClientJFournisseursOuverts;
+	}
+	
+	
+	/**
+	 * Calcul de 2 * fi - SOMMES...............
+	 * */
+	public int deuxfi_SommeDiffPositives(Fournisseur fournisseur_i) {
+		int somme = 0;
+		int sommetmp = 0;
+		
+		for (int idClient : getLesClients().keySet()) {//tous les clients
+			if(getLesClients().get(idClient)) { //client connecté
+				sommetmp = meilleurCoutClientFournisseursOuverts(idClient) - fournisseur_i.getCoutsClients().get(idClient);
+				if(sommetmp > 0) { //si c'est positif
+					somme += sommetmp;
+				}
+			}
+		}
+		return 2*(fournisseur_i.getCoutOuverture())-somme;
+	}
+	
+	/**
+	 * Beta :
+	 * */
+	
+	public float beta(Fournisseur fournisseur_i) {		
+		float min = Float.MAX_VALUE;
+		float sommeYDansS = 0;
+		float beta = deuxfi_SommeDiffPositives(fournisseur_i);
+		
+		clientsY = new HashMap<>();
+		
+		for (int idClientY : getLesClients().keySet()) {//tous les clients
+			if(!getLesClients().get(idClientY)) {//client pas connecté
+				getclientsY().put(idClientY, true); //augmente Y
+				sommeYDansS += fournisseur_i.getCoutsClients().get(idClientY); //ajout d'un client à Y
+				beta += sommeYDansS ; 
+				beta /= getclientsY().size(); //divise par |Y|
+				if (beta < min) {
+					min = beta;
+				}else {
+					getclientsY().remove(idClientY); //
+					beta=min;
+					//System.out.println(getclientsY().size());
+				}
+			}
+		}	
+		
+		return beta; 
+	}
+	
+	
+	public List<Fournisseur> glouton2() {
+		float minAlpha = Float.MAX_VALUE; 
+		float minBeta = Float.MAX_VALUE; 
+		float minBetaTmp = Float.MAX_VALUE;
+		int idClientAlpha = -1;
+		int idFournisseurtBeta = -1;
+		int nbClients = getFournisseurs().get(0).getCoutsClients().size();
+
+		while (nbClients > 0) {
+			
+			minBeta = Float.MAX_VALUE;
+			minBetaTmp = Float.MAX_VALUE;
+			
+			//alpha min
+			minAlpha = alpha();
+			
+			//beta min
+			for (int fournisseur_i : getFournisseursOuvertsFermes().keySet()) {
+				if(!getFournisseursOuvertsFermes().get(fournisseur_i)) {
+					minBetaTmp = beta(getFournisseurs().get(fournisseur_i));
+					if(minBetaTmp < minBeta) {
+						minBeta = minBetaTmp;
+						idFournisseurtBeta = fournisseur_i;
+					}
+				}
+			}
+			
+			if(minAlpha <= minBeta) {
+				int min = Integer.MAX_VALUE;
+				for (int idClient : getLesClients().keySet()) {
+					if(!getLesClients().get(idClient)) {
+						for (Fournisseur fournisseur: getFournisseursRetenus()) {
+							if(fournisseur.getCoutsClients().get(idClient) < min) {
+								min = fournisseur.getCoutsClients().get(idClient); 
+								idClientAlpha = idClient;
+							}
+						}
+					}
+				}
+				//System.out.println("alpha"+minAlpha);
+				getLesClients().put(idClientAlpha, true);
+				nbClients--;
+			}else {
+				beta(getFournisseurs().get(idFournisseurtBeta));
+				System.out.println(getclientsY().size());
+				for (int idClientY : getclientsY().keySet()) {
+					getLesClients().put(idClientY, true);
+					nbClients--;
+				}
+				//System.out.println("beta"+minBeta);
+				getFournisseursOuvertsFermes().put(idFournisseurtBeta, true);
+				getFournisseursRetenus().add(getFournisseurs().get(idFournisseurtBeta));
+			}
+			
+		}
+		
+		
+		return getFournisseursRetenus();
+	}
 	
 	public List<Fournisseur> getFournisseurs() {
 		return fournisseurs;
@@ -119,5 +312,31 @@ public class Algorithmes {
 	public void setFournisseursRetenus(List<Fournisseur> fournisseursRetenus) {
 		this.fournisseursRetenus = fournisseursRetenus;
 	}
+
+	public Map<Integer, Boolean> getLesClients() {
+		return lesClients;
+	}
+
+	public void setLesClients(Map<Integer, Boolean> lesClients) {
+		this.lesClients = lesClients;
+	}
+
+	public Map<Integer, Boolean> getFournisseursOuvertsFermes() {
+		return fournisseursOuvertsFermes;
+	}
+
+	public void setFournisseursOuvertsFermes(Map<Integer, Boolean> fournisseursOuvertsFermes) {
+		this.fournisseursOuvertsFermes = fournisseursOuvertsFermes;
+	}
+
+	public Map<Integer, Boolean> getclientsY() {
+		return clientsY;
+	}
+
+	public void setclientsY(Map<Integer, Boolean> clientsY) {
+		this.clientsY = clientsY;
+	}
+	
+	
 	
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -5,33 +5,61 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		
-		Lecteur lecteur = new Lecteur("/home/hotman/Documents/Workspace_Eclipse/APG_projet_fournisseurs/src/input/jeu_1/BildeKrarup/Eq/1/E1.7",
+		//chemin du fichier en premier argument, ne pas s'ocupper  du second argument
+		Lecteur lecteur = new Lecteur("/home/hotman/Documents/Workspace_Eclipse/APG_projet_fournisseurs/src/input/jeu_1/BildeKrarup/B/B1.2",
 				"/home/hotman/Documents/Workspace_Eclipse/APG_projet_fournisseurs/src/input/jeu_1/BildeKrarup/B/B1.7.opt");
 		
 		List<Fournisseur> fournisseurs = lecteur.mappeur();
-		lecteur.mappeurDonneesOpt();
+		/*
+		  lecteur.mappeurDonneesOpt();
+		 
 		
 		List<Integer> fournisseursOpt = lecteur.getDonneesOpt().stream().distinct().collect(Collectors.toList());
 		for (int i = 0; i < fournisseursOpt.size()-1; i++) {
 			System.out.println(fournisseursOpt.get(i));
 		}
-		
+		*/
 		Algorithmes algo = new Algorithmes(fournisseurs);
-		/**
+		/*
 		algo.getFournisseursRetenus().add(fournisseurs.get(13));
 		algo.getFournisseursRetenus().add(fournisseurs.get(43));
 		algo.getFournisseursRetenus().add(fournisseurs.get(32));
 		algo.getFournisseursRetenus().add(fournisseurs.get(14));
 		algo.getFournisseursRetenus().add(fournisseurs.get(10));
 		*/
-		algo.glouton1();
-		
+		algo.glouton2();
+		System.out.println(algo.getFournisseursOuvertsFermes());
 		System.out.println(algo.eval(algo.getFournisseursRetenus()));
 		
 		for (int i = 0; i < algo.getFournisseursRetenus().size(); i++) {
 			System.out.println(algo.getFournisseursRetenus().get(i).getId());
 		}
 		
+		System.out.println(algo.getLesClients());
+		
+		/*algo.trierCoutConnexion(fournisseurs);
+		
+		for (Fournisseur fournisseur : fournisseurs) {
+			System.out.print(fournisseur.getId()+ " ==> ");
+			for (int cout : fournisseur.getCoutsClients()) {
+				System.out.print(cout+" ; ");
+			}
+			System.out.println();
+		}*/
+		
+		//algo.glouton2();
+		/*int sommeY=0;
+		for (int idc : algo.calculY(algo.getFournisseurs().get(0)).keySet()) {
+			sommeY+= algo.getFournisseurs().get(0).getCoutsClients().get(idc);
+		}*/
+		/*
+		algo.glouton2();
+		System.out.println(algo.getFournisseursRetenus().size());	
+		//algo.glouton2();
+/*
+		for (int i = 0; i < algo.getFournisseursRetenus().size(); i++) {
+			System.out.println(i+" -"+algo.getFournisseursRetenus().get(i).getId());
+		}*/
 	}
 	
 }
